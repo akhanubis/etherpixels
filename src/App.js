6c423f13d5b13b609d0f74a3a47314559628db8f
Pablo Bianciotto <bianciottopablo@gmail.com>
2017-12-31 04:06:14 -0300

    Add canvas with hardcoded pixels

--- src/App.js
+++ src/App.js
@@ -1,23 +1,77 @@
 import React, { Component } from 'react'
+import Konva from 'konva'
 import CanvasContract from '../build/contracts/Canvas.json'
 import getWeb3 from './utils/getWeb3'
+import { Stage, Layer, Rect, Text } from "react-konva";
 
 import './css/oswald.css'
 import './css/open-sans.css'
 import './css/pure-min.css'
 import './App.css'
 
+class ColoredRect extends React.Component {
+  constructor(props) {
+    super(props)
+    this.pixel_size = 40
+    this.state = {
+      color_index: 0,
+      colors: [Konva.Util.getRandomColor(), Konva.Util.getRandomColor(), Konva.Util.getRandomColor(), Konva.Util.getRandomColor(), Konva.Util.getRandomColor(), Konva.Util.getRandomColor(), Konva.Util.getRandomColor()],
+      x: props.pixel.x,
+      y: props.pixel.y,
+      x_offset: props.canvas_size_x / 2,
+      y_offset: props.canvas_size_y / 2,
+    }
+  }
+  
+  handleClick = () => {
+    this.setState((prev_state) => {
+      return { color_index: (prev_state.color_index + 1) % prev_state.colors.length }
+    })
+  }
+  
+  current_color() {
+    return this.state.colors[this.state.color_index]
+  }
+  
+  render() {
+    return (
+      <Rect
+        x={this.state.x_offset + this.pixel_size * this.state.x}
+        y={this.state.y_offset + this.pixel_size * this.state.y}
+        width={this.pixel_size}
+        height={this.pixel_size}
+        fill={this.current_color()}
+        shadowBlur={5}
+        onClick={this.handleClick}
+      />
+    )
+  }
+}
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
-      pixels: null,
+      pixels: [
+        { x: 0, y: 0 },
+        { x: 0, y: -1 },
+        { x: 0, y: 1 },
+        { x: 1, y: 0 },
+        { x: 1, y: 1 },
+        { x: 1, y: -1 },
+        { x: -1, y: 0 },
+        { x: -1, y: 1 },
+        { x: -1, y: -1 }
+      ],
       web3: null,
-	  min: 0,
-	  max: 0,
-	  thresholds: [],
-	  genesis_block: null
+      contract_instance: null,
+      account: null,
+      min: 0,
+      max: 0,
+      thresholds: [],
+      genesis_block: null,
+      current_block: null
     }
   }
 
@@ -51,43 +105,46 @@ class App extends Component {
     const canvasContract = contract(CanvasContract)
     canvasContract.setProvider(this.state.web3.currentProvider)
 
-    // Declaring this for later so we can chain functions on SimpleStorage.
-    var contractInstance
-
     // Get accounts.
     this.state.web3.eth.getAccounts((error, accounts) => {
       canvasContract.deployed().then((instance) => {
-		  
-        contractInstance = instance
-
-		contractInstance.CurrentBoundaries().watch((error, result) => {
-		  if (!error) {
-			this.setState({ min: result.args['current_min'].toNumber(), max: result.args['current_max'].toNumber() })
-		  }
-		})
+        instance.CurrentBoundaries().watch((error, result) => {
+          if (error)
+            console.error(error)
+          else
+            this.setState({ min: result.args['current_min'].toNumber(), max: result.args['current_max'].toNumber() })	
+        })
 		
-		contractInstance.ThresholdsData.call().then(thresholds_data => {
-			var t_length = thresholds_data[0].length
-			var ts = []
-			for(var i = 0; i < t_length; i++)
-				ts.push( { threshold: thresholds_data[0][i].toNumber(), blocks_per_retarget: thresholds_data[1][i].toNumber() })
-			this.setState({
-				genesis_block: thresholds_data[2].toNumber(),
-				thresholds_length: t_length,
-				thresholds: ts
-			})
-		})
-		//this.state.web3.eth.estimateGas({from: accounts[0], to: contractInstance.address, amount: this.state.web3.toWei(1, "ether")}, (result) => { console.log(result)}) TODO VER ESTIMACION DE PAINT Y DEMAS
+        instance.ThresholdsData.call().then(thresholds_data => {
+          var t_length = thresholds_data[0].length
+          var ts = []
+          for(var i = 0; i < t_length; i++)
+            ts.push( { threshold: thresholds_data[0][i].toNumber(), blocks_per_retarget: thresholds_data[1][i].toNumber() })
+          this.setState({
+            genesis_block: thresholds_data[2].toNumber(),
+            thresholds_length: t_length,
+            thresholds: ts
+          })
+        })
+        
+        //this.state.web3.eth.estimateGas({from: accounts[0], to: contractInstance.address, amount: this.state.web3.toWei(1, "ether")}, (result) => { console.log(result)}) TODO VER ESTIMACION DE PAINT Y DEMAS
 		
-		setInterval(()=> {
-        contractInstance.Paint("0", "0", ['0xffffff', '0xff0000', '0x00ff00', '0x0000ff', '0x000000', '0x0f0f0f', '0xf0f0f0'], this.state.web3.fromAscii('pablo'), { from: accounts[0], value: "3000000000", gas: "2000000" })
-		}, 10000)
+        this.setState({ contract_instance: instance, account: accounts[0] })
+      })
+      
+      this.state.web3.eth.filter("latest").watch((error, block_hash) => {
+        this.state.web3.eth.getBlock(block_hash, (error, result) => {
+          if (error)
+            console.error(error)
+          else
+            this.setState({ current_block: result.number })  
+        })
       })
     })
   }
   
   prox_retarget(c_instance) {
-	var current_block_since_genesis = this.state.web3.eth.blockNumber - this.state.genesis_block
+	var current_block_since_genesis = this.state.current_block - this.state.genesis_block
 	var current_threshold = this.state.thresholds.findIndex(e => e.threshold > current_block_since_genesis)
 	if (current_threshold === -1)
 	  return 'inf'
@@ -98,19 +155,29 @@ class App extends Component {
     }
   }
   
+  paint(e) {
+	  e.preventDefault()
+	  this.state.contract_instance.Paint("0", "0", ['0xffffff', '0xff0000', '0x00ff00', '0x0000ff', '0x000000', '0x0f0f0f', '0xf0f0f0'], this.state.web3.fromAscii('pablo'), { from: this.state.account, value: "3000000000", gas: "2000000" })
+  }
   thresholds_fetched() {
 	return this.state.thresholds.length
   }
 
   render() {
-	let retarget_info = null
-	if (this.thresholds_fetched()) {
+    let retarget_info = null
+    if (this.thresholds_fetched()) {
       retarget_info = ([
-		<p>Genesis block: {this.state.genesis_block}</p>,
-		<p>Blocknumber: {this.state.web3.eth.blockNumber}</p>,
-		<p>Prox retarget en X bloques: {this.prox_retarget(this.state.contractInstance)}</p>
-		])
-    } else retarget_info = ''
+        <p>Genesis block: {this.state.genesis_block}</p>,
+        <p>Blocknumber: {this.state.current_block}</p>,
+        <p>Prox retarget en X bloques: {this.prox_retarget(this.state.contractInstance)}</p>
+      ])
+    }
+    else
+      retarget_info = ''
+    
+    let rects = this.state.pixels.map((p) => {
+      return <ColoredRect pixel={p} canvas_size_x={window.innerWidth} canvas_size_y={window.innerHeight}/>
+    })
     return (
       <div className="App">
         <nav className="navbar pure-menu pure-menu-horizontal">
@@ -122,15 +189,23 @@ class App extends Component {
             <div className="pure-u-1-1">
               <h1>Good to Go!</h1>
               <p>Your Truffle Box is installed and ready.</p>
+              <button onClick={this.paint.bind(this)}>
+                Paint
+              </button>
               <h2>Smart Contract Example</h2>
               <p>El minimo es: {this.state.min}</p>
-			  <p>El maximo es: {this.state.max}</p>
-			  {retarget_info}
+              <p>El maximo es: {this.state.max}</p>
+              {retarget_info}
             </div>
+            <Stage width={window.innerWidth} height={window.innerHeight}>
+              <Layer>
+                {rects}
+              </Layer>
+            </Stage>
           </div>
         </main>
       </div>
-    );
+    )
   }
 }
 
