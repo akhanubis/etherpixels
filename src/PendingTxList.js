69949f5ad5ba57bc191cdec71840cb719ca05380
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-01-29 21:07:09 -0300

    Use WeakMap to solve key in pixel batches

--- src/PendingTxList.js
+++ src/PendingTxList.js
@@ -5,9 +5,21 @@ import { FormGroup, Checkbox } from 'react-bootstrap'
 import './PendingTxList.css'
 
 class PendingTxList extends Component {
+  constructor(props) {
+    super(props)
+    this.weak_map_for_keys = new WeakMap()
+    this.weak_map_count = 0
+  }
+
   total_price() {
     let total_pixels = this.props.pending_txs.reduce((total, tx) => total.concat(tx.pixels), [])
     return this.props.gas_estimator.estimate_total(total_pixels)
+  }
+
+  key_for_tx(tx) {
+    if (!this.weak_map_for_keys.has(tx))
+      this.weak_map_for_keys.set(tx, ++this.weak_map_count)
+    return this.weak_map_for_keys.get(tx)
   }
 
   render() {
@@ -20,8 +32,8 @@ class PendingTxList extends Component {
           </FormGroup>
           <div className='pending-txs-inner-container'>
             {
-              this.props.pending_txs.map((tx, i) => {
-                return <PixelBatch key={i} batch={tx.pixels} is_full_callback={() => false} gas_estimator={this.props.gas_estimator} />
+              this.props.pending_txs.map(tx => {
+                return <PixelBatch key={this.key_for_tx(tx)} batch={tx.pixels} is_full_callback={() => false} gas_estimator={this.props.gas_estimator} />
               })
             }
           </div>
