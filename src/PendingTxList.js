322aa49cf8c55191a0e375d9c382b2e9619d5e1a
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-06 21:04:07 -0300

    Change pending tx enter transition to be a slide-in

--- src/PendingTxList.js
+++ src/PendingTxList.js
@@ -35,30 +35,36 @@ class PendingTxList extends PureComponent {
   }
 
   custom_height = () => {
-    return { height: `calc(100% - ${this.props.palette_height}px - 40px - 90px)`}
+    return { height: `calc(100% - ${this.props.palette_height}px - 40px - 90px)` }
   }
 
+  /* end of exit anim */
   willLeave = () => {
     return {
       opacity: spring(0, {stiffness: 66, damping: 14})
     }
   }
 
+  /* start of enter anim */
   willEnter = () => {
     return {
-      opacity: 0
+      left: -100,
+      opacity: 1
     }
   }
 
   on_start_style = () => {
-    return this.props.pending_txs.map(tx => ({ data: tx, key: this.key_for_tx(tx), style: { opacity: 0} }))
+    return this.props.pending_txs.map(tx => ({ data: tx, key: this.key_for_tx(tx), style: { opacity: 0 } }))
   }
 
-  active_style = () => this.props.pending_txs.map(tx => ({ data: tx, key: this.key_for_tx(tx), style: { opacity: spring(1, {stiffness: 66, damping: 14}) } }))
+  /* end of enter anim */
+  active_style = () => this.props.pending_txs.map(tx => ({ data: tx, key: this.key_for_tx(tx), style: { opacity: 1, left: spring(0, {stiffness: 66, damping: 14}) } }))
 
   tx_element = (key, tx) => {
     return React.createElement(PixelBatch, { current_panel: this.state.active_key /* trigger a render so collapsing works*/, panel_key: key.toString(), title: `Tx #${key}`, batch: tx.pixels, gas_estimator: this.props.gas_estimator })
   }
+
+  interpolated_to_css = style => ({ opacity: style.opacity, marginLeft: style.left + '%' })
 
   render() {
     return (
@@ -74,15 +80,15 @@ class PendingTxList extends PureComponent {
             willEnter={this.willEnter}
             styles={this.active_style()}
           >
-            {interpolatedStyles =>
+            {interpolatedStyles => (
               <div>
                 {interpolatedStyles.reverse().map(config => (
-                  <div key={config.key} style={config.style}>
+                  <div key={config.key} className="tx-panel-container" style={this.interpolated_to_css(config.style)}>
                     {this.tx_element(config.key, config.data)}
                   </div>
                 ))}
               </div>
-            }
+            )}
           </TransitionMotion>
         </PanelGroup>
       </div>
