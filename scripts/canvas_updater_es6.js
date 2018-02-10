10f59deb7f52c9bfc24053151c269bf13006174e
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-10 17:54:21 -0300

    Add pusher cluster to scripts

--- scripts/canvas_updater_es6.js
+++ scripts/canvas_updater_es6.js
@@ -37,6 +37,7 @@ let pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
   encrypted: true
 })
 
--- scripts/push_alert.js
+++ scripts/push_alert.js
@@ -5,6 +5,7 @@ let pusher = new Pusher({
   appId: process.env.PUSHER_APP_ID,
   key: process.env.PUSHER_APP_KEY,
   secret: process.env.PUSHER_APP_SECRET,
+  cluster: process.env.PUSHER_APP_CLUSTER,
   encrypted: true
 })
 
