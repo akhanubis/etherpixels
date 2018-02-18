6f521def7482f69b136508fedb49296d24a71823
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-18 15:36:15 -0300

    Remove ZeroClientProvider

--- src/utils/Infura.js
+++ src/utils/Infura.js
@@ -1,18 +1,24 @@
 import Web3 from 'web3'
-const ZeroClientProvider = require('./ZeroClientProvider.js')
+const ProviderEngine = require('web3-provider-engine')
+const RpcSubprovider = require('web3-provider-engine/subproviders/rpc.js')
+const FixtureSubprovider = require('web3-provider-engine/subproviders/fixture.js')
 
 var Infura = (() => {
   var get = () => {
-    return new Web3(ZeroClientProvider({
-              static: {
-                eth_syncing: false,
-                web3_clientVersion: 'ZeroClientProvider',
-              },
-              pollingInterval: 99999999, // not interested in polling for new blocks
-              rpcUrl: `https://${process.env.REACT_APP_INFURA_NETWORK}.infura.io/${ process.env.REACT_APP_INFURA_API_KEY }`,
-              // account mgmt
-              getAccounts: (cb) => cb(null, [])
+    let provider = new ProviderEngine()
+    provider.addProvider(new FixtureSubprovider({
+      web3_clientVersion: 'ProviderEngine/v0.0.0/javascript',
+      net_listening: true,
+      eth_hashrate: '0x00',
+      eth_mining: false,
+      eth_syncing: true,
     }))
+    provider.addProvider(new RpcSubprovider({
+      rpcUrl: `https://${location.href.includes('ropsten') ? 'ropsten' : 'mainnet'}.infura.io/${process.env.REACT_APP_INFURA_API_KEY}`,
+    }))
+    provider.on('error', err => console.error(err.stack))
+    provider.start()
+    return new Web3(provider)
   }
   
   return {
