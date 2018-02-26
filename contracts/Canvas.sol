60a84a556de008c6bb4c3f78a9d4efdcc9335707
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-26 02:31:45 -0300

    Contract changes

--- contracts/Canvas.sol
+++ contracts/Canvas.sol
@@ -1,8 +1,58 @@
 pragma solidity ^0.4.18;
-import "./UsingMortal.sol";
-import "./UsingCanvasBoundaries.sol";
 
-contract Canvas is usingMortal, usingCanvasBoundaries {
+contract usingOwnership {
+  address public contract_owner;
+
+  modifier onlyOwner() {
+    require(msg.sender == contract_owner);
+    _;
+  }
+
+  function usingOwnership() internal {
+    contract_owner = msg.sender;
+  }
+
+  function Withdraw(uint _amount) onlyOwner public {
+    if (_amount > this.balance)
+      _amount = this.balance;
+    contract_owner.transfer(_amount);
+  }
+
+  function TransferOwnership(address _new_owner) onlyOwner public {
+    require(_new_owner != address(0));
+    contract_owner = _new_owner;
+  }
+}
+
+contract usingCanvasBoundaries {
+  uint private g_block;
+  uint private max_max_index;
+  uint private max_block_number;
+  uint[] private halving;
+   
+  function usingCanvasBoundaries() internal {
+    g_block = block.number;
+    max_max_index = 4198401;
+    max_block_number = g_block + 3330049;
+    halving = [g_block + 16384, g_block + 81920, g_block + 770048];
+  }
+
+  function max_index() internal view returns(uint m_index) {
+    if (block.number > max_block_number)
+      return max_max_index;
+    uint delta = block.number - g_block;
+    return delta +
+    ((block.number <= halving[0]) ? delta : halving[0] - g_block) +
+    ((block.number <= halving[1]) ? delta : halving[1] - g_block) +
+    ((block.number <= halving[2]) ? delta : halving[2] - g_block);
+  }
+
+  function HalvingInfo() public view returns(uint genesis_block, uint[] halving_array) {
+    return (g_block, halving);
+  }
+}
+
+contract Canvas is usingOwnership, usingCanvasBoundaries {
   uint private starting_price = 5000000000000; /* 5000 gwei */
 
   /* packed to 32 bytes */
@@ -35,8 +85,8 @@ contract Canvas is usingMortal, usingCanvasBoundaries {
     return starting_price;
   }
 
-  function LowerStartingPrice(uint _new_starting_price) public {
-    assert(msg.sender == contract_owner && _new_starting_price < starting_price);
+  function LowerStartingPrice(uint _new_starting_price) onlyOwner public {
+    require(_new_starting_price < starting_price);
     starting_price = _new_starting_price;
   }
   
@@ -58,7 +108,7 @@ contract Canvas is usingMortal, usingCanvasBoundaries {
         p.owner = msg.sender;
         PixelPainted(_index, msg.sender, old_owner, p.price, _color);
         if (old_owner != address(0))
-          old_owner.transfer(_paid * 98 / 100);
+          old_owner.send(_paid * 98 / 100); /* not using transfer to avoid old_owner locking pixel by buying it from a contract that reverts when receiving funds */
       }
     }
 	}
