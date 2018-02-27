f3673a1ea42e48b84c0397e13506f109704991e5
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-27 17:26:10 -0300

    Wording change

--- src/ComingSoon.js
+++ src/ComingSoon.js
@@ -6,7 +6,7 @@ class ComingSoon extends PureComponent {
     return (
       <div className="coming-soon-container">
         <h1>Coming soon...</h1>
-        <p>In the meantime, you can play around with the Ropsten version of the app by switching Metamask to the Ropsten network</p>
+        <p>In the meantime, you can play around with the beta version of the app by switching Metamask to the Ropsten network</p>
         <img src="assets/metamask_ropsten.png"/>
         <p>Need a Ropsten faucet? Try <a href="https://faucet.metamask.io/" target="_blank">https://faucet.metamask.io/</a></p>
       </div>
