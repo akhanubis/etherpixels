2cab3161f9898ed3df04ed10a6d51a80b3d439ed
Pablo Bianciotto <bianciottopablo@gmail.com>
2018-02-28 18:52:38 -0300

    Add About info

--- src/About.js
+++ src/About.js
@@ -12,7 +12,47 @@ class About extends PureComponent {
   render() {
     return (
       <div className="right-panel about-panel" style={this.style()}>
-        hoalaaa
+        <div className="about-panel-inner">
+          <h2 className="about-panel-title">About Etherpixels</h2>
+          <p>
+            Built by <a href="https://github.com/akhanubis" target="_blank">akhanubis</a>
+          </p>
+          <p>
+            Styled by <a href="https://www.behance.net/violetazulado" target="_blank">violetazulado</a>
+          </p>
+          <p>
+            Reach us at <a href="mailto:team@etherpixels.co">team@etherpixels.co</a>
+          </p>
+          <p>
+            Built using
+          </p>
+          <div className="built-using">
+            <div>
+              <div className="built-using-icon">
+                <i className="fab fa-node-js"/>
+              </div>
+              <div className="built-using-name">nodejs</div>
+            </div>
+            <div>
+              <div className="built-using-icon">
+                <i className="fab fa-react"/>
+              </div>
+              <div className="built-using-name">React</div>
+            </div>
+          </div>
+          <p>
+            Powered by
+          </p>
+          <div className="built-using">
+            <div>
+              <div className="built-using-icon">
+                <i className="fab fa-ethereum"/>
+              </div>
+              <div className="built-using-name">Ethereum</div>
+            </div>
+          </div>
+          <img src="assets/builtfirebase.png" width="250px" className="firebase-image"/>
+        </div>
       </div>
     )
   }
